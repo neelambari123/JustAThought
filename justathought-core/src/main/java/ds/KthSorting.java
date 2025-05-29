622d710779280079c908930eb45366a82b9e3cc4
neelambari123 <82968999+neelambari123@users.noreply.github.com>
2025-05-29 20:50:12 +0530

    added somthing

---
+++ justathought-core/src/main/java/ds/KthSorting.java
@@ -0,0 +1,29 @@
+package ds;
+
+import java.util.Comparator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.PriorityQueue;
+
+public class KthSorting {
+	
+	public static List<Integer> findKthLargestElementsFromList(List<Integer> integers,int k){
+		PriorityQueue<Integer> priorityQueue=new PriorityQueue<>(Comparator.reverseOrder());
+		priorityQueue.addAll(integers);
+		LinkedList<Integer> linkedList=new LinkedList<>();
+		while(linkedList.size()!=k) {
+			linkedList.add(priorityQueue.peek());
+			priorityQueue.remove();
+		}
+	
+		return linkedList.stream().toList();
+	}
+	
+	public static void main(String[] args) {
+		List<Integer> integers=List.of(20,11,1,2,3,4,5,6,7,8,9);
+		System.out.println(findKthLargestElementsFromList(integers, 5));
+		System.out.print("kabinarayan");
+		
+	}
+
+}
